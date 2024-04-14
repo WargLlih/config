4fe5d2f11c2c5a9f93c4f2cf6caff992bccbea8f
Daniel Junior <39025412+WargLlih@users.noreply.github.com>
2024-04-14 17:27:55 -0300

    Created config setup

---
+++ wl-pkgs-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo pacman -Sy git neovim wl-clipboard zsh polkit-gnome waybar adobe-source-han-sans-cn-fonts filelight zsh-autosuggestions zsh-syntax-highlighting ripgrep unzip starship swappy grim slurp ttf-jetbrains-mono-nerd noto-fonts-emoji wofi pamixer mako brightnessctl lazygit go btop sof-firmware hyprpaper bat lf
+
+yay -S bluetuith swaylock-effects spotify
+
+chsh -s /bin/zsh
